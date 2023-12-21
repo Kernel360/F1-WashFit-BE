5bcf29cd5685d7241b8de29f7353920c2c67b80a
HyunJunSon <jjang3a@gmail.com>
2023-12-21 18:12:50 +0900

    test: manually deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,7 @@ USER_NAME=${SECRET_VULTR_USER_NAME}
 
 # SSH 키
 # 이 변수는 GitHub Actions에서 set-env 액션을 사용하여 설정됩니다.
-SSH_KEY=${{ SECRET_VULTR_SSH_KEY }}
+SSH_KEY=${SECRET_VULTR_SSH_KEY}
 
 # Vultr 인스턴스의 Docker를 중지합니다. (필요한 경우)
 ssh -i $SSH_KEY $USER_NAME@$VULTR_IP "systemctl stop docker"
