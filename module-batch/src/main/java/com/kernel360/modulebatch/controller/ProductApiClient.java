01215c18c36a05849434d8b566b5eaf15a6ec49b
김찬규 <kjkj5868@gmail.com>
2024-01-05 19:06:51 +0900

    chore : 필요없는 파일 삭제 혹은 디렉터리 이동

--- module-batch/src/main/java/com/kernel360/modulebatch/controller/ProductApiClient.java
+++
@@ -1,9 +0,0 @@
-package com.kernel360.modulebatch.controller;
-
-import org.springframework.http.ResponseEntity;
-public interface ProductApiClient {
-
-    ResponseEntity<?> getRawData(int page);
-
-    String buildUrl(int pageNumber);
-}
