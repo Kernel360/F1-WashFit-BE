0be43488def5e58c1348bfdbfd98e3681c8f254d
김찬규 <kjkj5868@gmail.com>
2024-01-08 16:49:18 +0900

    refactor : 작업당 처리할 요청의 수를 10번으로 제한
    따라서 한 작업당 10 * 20 = 200개 씩 적재하도록 변경되었음

--- module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductApiJobConfig.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductApiJobConfig.java
@@ -1,6 +1,5 @@
 package com.kernel360.modulebatch.job;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kernel360.modulebatch.dto.ReportedProductDto;
 import com.kernel360.modulebatch.service.ReportedProductService;
 import java.util.List;
@@ -10,10 +9,9 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
@@ -23,22 +21,22 @@ import org.springframework.transaction.PlatformTransactionManager;
 @RequiredArgsConstructor
 public class ReportedProductApiJobConfig {
 
-    private final ReportedProductService apiService;
-
+    private final ReportedProductService service;
 
     @Bean
     public Job fetchReportedProductListJob(JobRepository jobRepository, Step fetchProduct) {
+        log.info("Job Build Configuration");
         return new JobBuilder("fetchReportedProductListJob", jobRepository)
                 .start(fetchProduct)
+                .incrementer(new RunIdIncrementer())
                 .build();
     }
 
     @Bean
-    public Step fetchProduct(JobRepository jobRepository, PlatformTransactionManager transactionManager)
-            throws JsonProcessingException {
-
+    public Step fetchProduct(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        log.info("Step Build Configuration");
         return new StepBuilder("fetchReportedProductStep", jobRepository)
-                .<List<ReportedProductDto>, List<ReportedProductDto>>chunk(20, transactionManager)
+                .<List<ReportedProductDto>, List<ReportedProductDto>>chunk(10, transactionManager)
                 .reader(productListItemReader()) // API 요청, 응답을 DTO 리스트로 반환
                 .writer(productListItemWriter()) // DTO 리스트 입력, 저장
                 .build();
@@ -46,14 +44,14 @@ public class ReportedProductApiJobConfig {
 
     @Bean
     @StepScope
-    public ItemReader<List<ReportedProductDto>> productListItemReader() throws JsonProcessingException {
-        return new ReportedProductListItemReader(apiService);
+    public ReportedProductListItemReader productListItemReader() {
+        return new ReportedProductListItemReader(service);
     }
 
     @Bean
     @StepScope
-    public ItemWriter<List<ReportedProductDto>> productListItemWriter() {
-        return new ReportedProductListItemWriter(apiService);
+    public ReportedProductListItemWriter productListItemWriter() {
+        return new ReportedProductListItemWriter(service);
     }
 
 
--- module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductListItemReader.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductListItemReader.java
@@ -6,35 +6,59 @@ import com.kernel360.modulebatch.dto.ReportedProductDto;
 import com.kernel360.modulebatch.service.ReportedProductService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.annotation.AfterStep;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemReader;
 
 @Slf4j
 public class ReportedProductListItemReader implements ItemReader<List<ReportedProductDto>> {
+    private final static String PAGE_KEY = "pageKey";
+    private final static String START_PAGE_KEY = "startPageKey";
+    private static int MAX_PAGES_PER_JOB = 10;
     private final ReportedProductService service;
     private final ReportedProductClient client;
     private int maxPageNumber;
-    private int pageNumber = 0;
+    private int nextPage;
 
 
-    public ReportedProductListItemReader(ReportedProductService service) throws JsonProcessingException {
+    public ReportedProductListItemReader(ReportedProductService service) {
         this.client = new ReportedProductClient();
         this.service = service;
     }
 
+    @BeforeStep
+    public void beforeStep(StepExecution stepExecution) {
+        ExecutionContext executionContext = stepExecution.getExecutionContext();
+        if (executionContext.containsKey(PAGE_KEY)) {
+            nextPage = executionContext.getInt(PAGE_KEY);
+            executionContext.putInt(START_PAGE_KEY, nextPage);
+        } else {
+            nextPage = -1;
+        }
+    }
+
     @Override
     public List<ReportedProductDto> read() throws JsonProcessingException {
-        pageNumber++;
-        if (pageNumber == maxPageNumber + 1) {
+        nextPage++;
+        if (nextPage == maxPageNumber + 1 || MAX_PAGES_PER_JOB-- < 0) { //
             return null;
         }
 
-        log.info("Fetch Page = {}", pageNumber);
-        String xmlResponse = client.getXmlResponse(pageNumber);
+        log.info("Fetch Page = {}", nextPage);
+        String xmlResponse = client.getXmlResponse(nextPage);
         maxPageNumber = service.getTotalPageCount(xmlResponse);
         log.info("Response accepted : {}", xmlResponse);
 
         return service.deserializeXml2ListDto(xmlResponse)
                       .reportedProductDtoList();
+    }
+
+    @AfterStep
+    public void afterStep(StepExecution stepExecution) {
+        ExecutionContext executionContext = stepExecution.getExecutionContext();
+        executionContext.putInt(PAGE_KEY, nextPage);
     }
 
 }
