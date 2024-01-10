c4f42da78b1a628163dc9b9df36250f82c1670f5
김찬규 <kjkj5868@gmail.com>
2024-01-10 11:24:23 +0900

    refactor : 명확한 의미전달을 위해 메서드 명 변경

--- module-domain/src/main/java/com/kernel360/ecolife/repository/ReportedProductRepository.java
+++ module-domain/src/main/java/com/kernel360/ecolife/repository/ReportedProductRepository.java
@@ -1,6 +1,7 @@
 package com.kernel360.ecolife.repository;
 
 import com.kernel360.ecolife.entity.ReportedProduct;
+import com.kernel360.ecolife.entity.ReportedProductId;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReportedProductRepository extends JpaRepository<ReportedProduct, String> {
-    @Query("SELECT rp FROM ReportedProduct rp WHERE rp.productMasterId = :productMasterId")
-    Optional<ReportedProduct> findByProductMasterId(@Param("productMasterId") String productMasterId);
+    @Query("SELECT rp FROM ReportedProduct rp WHERE rp.id = :id")
+    Optional<ReportedProduct> findById(@Param("id") ReportedProductId reportedProductId);
 
 }
