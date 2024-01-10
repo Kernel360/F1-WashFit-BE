863c7d1a19668a7f8f027b4999a62982a38337b0
김찬규 <kjkj5868@gmail.com>
2024-01-10 11:34:22 +0900

    refactor : 샘플데이터 확보를 위해 100 페이지까지만 요청하도록 변경, 메서드 분리

--- module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductListItemReader.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductListItemReader.java
@@ -7,20 +7,17 @@ import com.kernel360.modulebatch.service.ReportedProductService;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.annotation.AfterStep;
 import org.springframework.batch.core.annotation.BeforeStep;
-import org.springframework.batch.item.ExecutionContext;
 import org.springframework.batch.item.ItemReader;
 
 @Slf4j
 public class ReportedProductListItemReader implements ItemReader<List<ReportedProductDto>> {
-    private final static String PAGE_KEY = "pageKey";
-    private final static String START_PAGE_KEY = "startPageKey";
-    private static int MAX_PAGES_PER_JOB = 10;
+
+    private static int MAX_PAGES_PER_JOB;
     private final ReportedProductService service;
     private final ReportedProductClient client;
     private int maxPageNumber;
-    private int nextPage;
+    private int nextPage = -1;
 
 
     public ReportedProductListItemReader(ReportedProductService service) {
@@ -28,37 +25,39 @@ public class ReportedProductListItemReader implements ItemReader<List<ReportedPr
         this.service = service;
     }
 
+    // FIXME :: 몇 페이지까지 요청을 해서 데이터를 가져왔는지 저장하고 다음 스케쥴에 잡을 실행할 때 진행하지 않은 부분만 작업하도록 변경이 필요
     @BeforeStep
     public void beforeStep(StepExecution stepExecution) {
-        ExecutionContext executionContext = stepExecution.getExecutionContext();
-        if (executionContext.containsKey(PAGE_KEY)) {
-            nextPage = executionContext.getInt(PAGE_KEY);
-            executionContext.putInt(START_PAGE_KEY, nextPage);
-        } else {
-            nextPage = -1;
-        }
+        MAX_PAGES_PER_JOB = 100;
+        // nextPage 를 중앙저장장치에 저장해놓아야 함...
+        //         ExecutionContext executionContext = stepExecution.getExecutionContext();
+        //        if (executionContext.containsKey(PAGE_KEY)) {
+        //            nextPage = executionContext.getInt(PAGE_KEY);
+        //            executionContext.putInt(START_PAGE_KEY, nextPage);
+        //        } else {
+        //            nextPage = -1;
+        //        }
     }
 
     @Override
     public List<ReportedProductDto> read() throws JsonProcessingException {
         nextPage++;
-        if (nextPage == maxPageNumber + 1 || MAX_PAGES_PER_JOB-- < 0) { //
+        if (nextPage == maxPageNumber + 1 || MAX_PAGES_PER_JOB-- <= 0) {
             return null;
         }
 
-        log.info("Fetch Page = {}", nextPage);
-        String xmlResponse = client.getXmlResponse(nextPage);
+        String xmlResponse = fetchXmlResponse();
+
         maxPageNumber = service.getTotalPageCount(xmlResponse);
-        log.info("Response accepted : {}", xmlResponse);
 
         return service.deserializeXml2ListDto(xmlResponse)
                       .reportedProductDtoList();
     }
 
-    @AfterStep
-    public void afterStep(StepExecution stepExecution) {
-        ExecutionContext executionContext = stepExecution.getExecutionContext();
-        executionContext.putInt(PAGE_KEY, nextPage);
+    private String fetchXmlResponse() {
+        log.info("Fetch Page = {}", nextPage);
+        String xmlResponse = client.getXmlResponse(nextPage);
+        log.info("Response accepted : {}", xmlResponse);
+        return xmlResponse;
     }
-
 }
