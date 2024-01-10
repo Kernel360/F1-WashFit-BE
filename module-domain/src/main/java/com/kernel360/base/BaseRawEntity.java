eb5857f638ab55894e98411c440108056c5ff013
김찬규 <kjkj5868@gmail.com>
2024-01-10 11:23:59 +0900

    refactor : 엔티티 업데이트 로직 실행시 발생하는 null or transient 문제 회피를 위해서 속성 변경

--- module-domain/src/main/java/com/kernel360/base/BaseRawEntity.java
+++ module-domain/src/main/java/com/kernel360/base/BaseRawEntity.java
@@ -15,10 +15,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseRawEntity {
-
+    // FIXME :: createdAt 자동 생성시, update 로직 진행중에 null or transient 문제가 발생. 임시방편으로 직접 지정하였으나 이후 수정 필요
     @Column(name = "created_at", nullable = false)
     @CreatedDate
-    private LocalDate createdAt;
+    private LocalDate createdAt = LocalDate.now();
 
     @Column(name = "created_by", nullable = false)
     @CreatedBy
