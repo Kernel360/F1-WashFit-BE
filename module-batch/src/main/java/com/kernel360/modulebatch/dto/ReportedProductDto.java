b980ca648092992504c74e04a8d09e05ecf178ba
김찬규 <kjkj5868@gmail.com>
2024-01-10 11:37:30 +0900

    refactor : 복합키 전환에 따른 dto 변경

--- module-batch/src/main/java/com/kernel360/modulebatch/dto/ReportedProductDto.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/dto/ReportedProductDto.java
@@ -2,6 +2,7 @@ package com.kernel360.modulebatch.dto;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.kernel360.ecolife.entity.ReportedProduct;
+import com.kernel360.ecolife.entity.ReportedProductId;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -9,22 +10,16 @@ public record ReportedProductDto(
 
         @JacksonXmlProperty(localName = "mst_id")
         String productMasterId,
-
         @JacksonXmlProperty(localName = "prdt_nm")
         String productName,
-
         @JacksonXmlProperty(localName = "slfsfcfst_no")
         String safetyReportNumber,
-
         @JacksonXmlProperty(localName = "item")
         String item,
-
         @JacksonXmlProperty(localName = "est_no")
         int estNumber,
-
         @JacksonXmlProperty(localName = "reg_date")
         String registeredDate,
-
         @JacksonXmlProperty(localName = "comp_nm")
         String companyName
 ) {
@@ -42,11 +37,10 @@ public record ReportedProductDto(
 
     public static ReportedProduct toEntity(ReportedProductDto reportedProductDto) {
         return ReportedProduct.of(
-                reportedProductDto.productMasterId,
+                new ReportedProductId(reportedProductDto.productMasterId, reportedProductDto.estNumber),
                 reportedProductDto.productName,
                 reportedProductDto.safetyReportNumber,
                 reportedProductDto.item,
-                reportedProductDto.estNumber,
                 LocalDate.parse(reportedProductDto.registeredDate, DateTimeFormatter.ofPattern("yyyyMMdd"))
                          .atStartOfDay(),
                 reportedProductDto.companyName);
