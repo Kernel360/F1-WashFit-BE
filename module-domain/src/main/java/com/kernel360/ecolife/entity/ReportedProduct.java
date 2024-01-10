aa90653aea0285b1225e9397d52a240000c8fe3a
김찬규 <kjkj5868@gmail.com>
2024-01-10 11:26:38 +0900

    refactor : pk를 복합키로 변경, 제품 세부사항 정보 업데이트를 위한 생성자와 생성자 호출 메서드 추가
    기존 pk : productMasterId
    변경 pk : productMasterId + estNumber

--- module-domain/src/main/java/com/kernel360/ecolife/entity/ReportedProduct.java
+++ module-domain/src/main/java/com/kernel360/ecolife/entity/ReportedProduct.java
@@ -1,21 +1,22 @@
 package com.kernel360.ecolife.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReportedProduct extends ReportedProductDetails {
 
-    @Id
-    @Column(name = "mst_id")
-    private String productMasterId;
+    @EmbeddedId
+    @Column(name = "composite_id")
+    private ReportedProductId id;
 
     @Column(name = "prdt_nm")
     private String productName;
@@ -26,41 +27,148 @@ public class ReportedProduct extends ReportedProductDetails {
     @Column(name = "item")
     private String item;
 
-    @Column(name = "est_no")
-    private int estNumber;
-
     @Column(name = "reg_date")
     private LocalDateTime registeredDate;
 
     @Column(name = "comp_nm")
     private String companyName;
 
-    private ReportedProduct(String productMasterId,
+
+    private ReportedProduct(ReportedProductId reportedProductId,
                             String productName,
                             String safetyReportNumber,
                             String item,
-                            int estNumber,
                             LocalDateTime registeredDate,
                             String companyName
     ) {
-        this.productMasterId = productMasterId;
+        this.id = reportedProductId;
         this.productName = productName;
         this.safetyReportNumber = safetyReportNumber;
         this.item = item;
-        this.estNumber = estNumber;
         this.registeredDate = registeredDate;
         this.companyName = companyName;
     }
 
-    public static ReportedProduct of(String productMasterId,
+    private ReportedProduct(ReportedProductId reportedProductId,
+                            String productName,
+                            String safetyReportNumber,
+                            String item,
+                            LocalDateTime registeredDate,
+                            String companyName,
+                            String inspectedOrganization,
+                            String productType,
+                            LocalDateTime issuedDate,
+                            String isRenewed,
+                            String safeStandard,
+                            LocalDateTime expireDate,
+                            String upperItem,
+                            String kidProtectPackage,
+                            String productDefinition,
+                            String productPropose,
+                            String distributeLimit,
+                            String weightAndBulk,
+                            String icePoint,
+                            String standardUseQuantity,
+                            String useMethod,
+                            String usageAttentionReport,
+                            String firstAid,
+                            String mainSubstance,
+                            String preservative,
+                            String allergicSubstance,
+                            String Surfactant,
+                            String otherSubstance,
+                            String FluorescentWhiteningAgent,
+                            String manufacture,
+                            String manufactureMethod,
+                            String manufactureNation,
+                            String companyAddress,
+                            String companyTel,
+                            String importedCompanyName,
+                            String importedCompanyAddress,
+                            String importedCompanyTel,
+                            String vendorName,
+                            String vendorCompanyAddress,
+                            String vendorCompanyTel) {
+        super(inspectedOrganization, productType, issuedDate, isRenewed, safeStandard,
+                expireDate, upperItem, kidProtectPackage, productDefinition, productPropose,
+                distributeLimit, weightAndBulk, icePoint, standardUseQuantity, useMethod,
+                usageAttentionReport, firstAid, mainSubstance, preservative, allergicSubstance,
+                Surfactant, otherSubstance, FluorescentWhiteningAgent, manufacture, manufactureMethod,
+                manufactureNation, companyAddress, companyTel, importedCompanyName, importedCompanyAddress,
+                importedCompanyTel, vendorName, vendorCompanyAddress, vendorCompanyTel);
+        this.id = reportedProductId;
+        this.productName = productName;
+        this.safetyReportNumber = safetyReportNumber;
+        this.item = item;
+        this.registeredDate = registeredDate;
+        this.companyName = companyName;
+    }
+
+
+    public static ReportedProduct of(ReportedProductId reportedProductId,
                                      String productName,
                                      String safetyReportNumber,
                                      String item,
-                                     int estNumber,
                                      LocalDateTime registeredDate,
                                      String companyName) {
-        return new ReportedProduct(productMasterId, productName, safetyReportNumber, item, estNumber, registeredDate,
-                companyName);
+        return new ReportedProduct(
+                reportedProductId,
+                productName,
+                safetyReportNumber,
+                item,
+                registeredDate,
+                companyName
+        );
+    }
+
+    public static ReportedProduct of(ReportedProductId reportedProductId,
+                                     String productName,
+                                     String safetyReportNumber,
+                                     String item,
+                                     LocalDateTime registeredDate,
+                                     String companyName,
+                                     String inspectedOrganization,
+                                     String productType,
+                                     LocalDateTime issuedDate,
+                                     String isRenewed,
+                                     String safeStandard,
+                                     LocalDateTime expireDate,
+                                     String upperItem,
+                                     String kidProtectPackage,
+                                     String productDefinition,
+                                     String productPropose,
+                                     String distributeLimit,
+                                     String weightAndBulk,
+                                     String icePoint,
+                                     String standardUseQuantity,
+                                     String useMethod,
+                                     String usageAttentionReport,
+                                     String firstAid,
+                                     String mainSubstance,
+                                     String preservative,
+                                     String allergicSubstance,
+                                     String Surfactant,
+                                     String otherSubstance,
+                                     String FluorescentWhiteningAgent,
+                                     String manufacture,
+                                     String manufactureMethod,
+                                     String manufactureNation,
+                                     String companyAddress,
+                                     String companyTel,
+                                     String importedCompanyName,
+                                     String importedCompanyAddress,
+                                     String importedCompanyTel,
+                                     String vendorName,
+                                     String vendorCompanyAddress,
+                                     String vendorCompanyTel) {
+        return new ReportedProduct(reportedProductId, productName, safetyReportNumber, item, registeredDate,
+                companyName, inspectedOrganization, productType, issuedDate, isRenewed, safeStandard,
+                expireDate, upperItem, kidProtectPackage, productDefinition, productPropose,
+                distributeLimit, weightAndBulk, icePoint, standardUseQuantity, useMethod,
+                usageAttentionReport, firstAid, mainSubstance, preservative, allergicSubstance,
+                Surfactant, otherSubstance, FluorescentWhiteningAgent, manufacture, manufactureMethod,
+                manufactureNation, companyAddress, companyTel, importedCompanyName, importedCompanyAddress,
+                importedCompanyTel, vendorName, vendorCompanyAddress, vendorCompanyTel);
     }
 
 }
