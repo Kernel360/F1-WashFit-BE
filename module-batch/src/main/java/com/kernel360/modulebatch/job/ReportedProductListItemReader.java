dca9d1245d9b25f8e89366e46d9c10a26d91ef31
김찬규 <kjkj5868@gmail.com>
2024-01-10 12:17:22 +0900

    refactor : 사용하지 않는 StepExecution 관련 코드 삭제

--- module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductListItemReader.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/job/ReportedProductListItemReader.java
@@ -27,16 +27,8 @@ public class ReportedProductListItemReader implements ItemReader<List<ReportedPr
 
     // FIXME :: 몇 페이지까지 요청을 해서 데이터를 가져왔는지 저장하고 다음 스케쥴에 잡을 실행할 때 진행하지 않은 부분만 작업하도록 변경이 필요
     @BeforeStep
-    public void beforeStep(StepExecution stepExecution) {
+    public void beforeStep() {
         MAX_PAGES_PER_JOB = 100;
-        // nextPage 를 중앙저장장치에 저장해놓아야 함...
-        //         ExecutionContext executionContext = stepExecution.getExecutionContext();
-        //        if (executionContext.containsKey(PAGE_KEY)) {
-        //            nextPage = executionContext.getInt(PAGE_KEY);
-        //            executionContext.putInt(START_PAGE_KEY, nextPage);
-        //        } else {
-        //            nextPage = -1;
-        //        }
     }
 
     @Override
