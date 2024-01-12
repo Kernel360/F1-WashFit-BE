e46229716a7c9b008f8c6c7aaa07d5b0b800ee6b
김찬규 <kjkj5868@gmail.com>
2024-01-12 14:56:32 +0900

    refactor :: Brand 엔티티에 companyName 필드 추가

--- module-domain/src/main/java/com/kernel360/brand/entity/Brand.java
+++ module-domain/src/main/java/com/kernel360/brand/entity/Brand.java
@@ -1,26 +1,43 @@
 package com.kernel360.brand.entity;
 
 import com.kernel360.base.BaseEntity;
-import jakarta.persistence.*;
+import com.kernel360.product.entity.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.Getter;
-import lombok.Setter;
 
 @Getter
-@Setter
 @Entity
 @Table(name = "brand")
 public class Brand extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "brand_id_gen")
-    @SequenceGenerator(name = "brand_id_gen", sequenceName = "brand_brand_no_seq")
     @Column(name = "brand_no", nullable = false)
+    @SequenceGenerator(name = "brand_id_gen", sequenceName = "brand_brand_no_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "brand_id_gen")
     private Integer brandNo;
-    @Column(name = "brand_name", nullable = false, length = Integer.MAX_VALUE)
+
+    @Column(name = "brand_name", nullable = false)
     private String brandName;
+
+    @Column(name = "company_name", nullable = false)
+    private String companyName;
 
     @Column(name = "description", length = Integer.MAX_VALUE)
     private String description;
 
-    @Column(name = "nation_name", length = Integer.MAX_VALUE)
+    @Column(name = "nation_name")
     private String nationName;
+
+    @OneToMany(mappedBy = "brand")
+    private List<Product> productList = new ArrayList<>();
+
+
 }
