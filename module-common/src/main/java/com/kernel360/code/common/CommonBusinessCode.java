faea3f1f44baba3e6509f165e37ed560a5910d76
Younglong <kyl8555@gmail.com>
2024-01-15 14:26:29 +0900

    feat: api response 관련 내용 추가
    - BusinessCode 인터페이스로 추가

---
+++ module-common/src/main/java/com/kernel360/code/common/CommonBusinessCode.java
@@ -0,0 +1,32 @@
+package com.kernel360.code.common;
+
+import com.kernel360.code.ErrorCode;
+
+public enum CommonBusinessCode implements ErrorCode {
+    SAVED(200, "B001", "저장이 왼료되었습니다.");
+
+    private int status;
+    private final String code;
+    private final String message;
+
+    CommonBusinessCode(int status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public int getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
