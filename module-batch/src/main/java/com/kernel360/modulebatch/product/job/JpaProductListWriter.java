23058fd130531463a08819c6ff63f645f6ef8310
김찬규 <kjkj5868@gmail.com>
2024-01-17 12:27:30 +0900

    feat :: JpaWriter 를 List<Entity> 에도 사용할 수 있도록 JpaListWriter 구현

---
+++ module-batch/src/main/java/com/kernel360/modulebatch/product/job/JpaProductListWriter.java
@@ -0,0 +1,24 @@
+package com.kernel360.modulebatch.product.job;
+
+import java.util.List;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.database.JpaItemWriter;
+
+public class JpaProductListWriter<T> extends JpaItemWriter<List<T>> {
+    private final JpaItemWriter<T> jpaItemWriter;
+
+    public JpaProductListWriter(JpaItemWriter<T> jpaItemWriter) {
+        this.jpaItemWriter = jpaItemWriter;
+    }
+
+    @Override
+    public void write(Chunk<? extends List<T>> items) {
+        Chunk<T> totalChunk = new Chunk<>();
+
+        for (List<T> list : items) {
+            totalChunk.addAll(list);
+        }
+        jpaItemWriter.write(totalChunk);
+    }
+}
+
