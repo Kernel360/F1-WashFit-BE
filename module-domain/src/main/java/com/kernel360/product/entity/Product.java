bd46cb593939ba4d4a3314a7a0f372c4bbfee601
김찬규 <kjkj5868@gmail.com>
2024-01-17 12:10:08 +0900

    feat :: Product 엔티티에 필드 추가, module-batch 에서 사용할 ProductDto 추가, ProductRepository 메서드 추가

--- module-domain/src/main/java/com/kernel360/product/entity/Product.java
+++ module-domain/src/main/java/com/kernel360/product/entity/Product.java
@@ -12,6 +12,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -25,7 +26,7 @@ public class Product extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_id_gen")
     @SequenceGenerator(name = "product_id_gen", sequenceName = "product_product_no_seq", allocationSize = 50)
     @Column(name = "product_no", nullable = false)
-    private Integer productNo;
+    private Long productNo;
 
     @Column(name = "product_name", nullable = false)
     private String productName;
@@ -36,14 +37,72 @@ public class Product extends BaseEntity {
     @Column(name = "description", length = Integer.MAX_VALUE)
     private String description;
 
-    @Column(name = "declare_no", nullable = false)
-    private String declareNo;
-
     @Column(name = "is_violation", nullable = false)
     private Boolean isViolation = false;
 
     @Column(name = "view_count", nullable = false)
     private Integer viewCount;
+
+    @Column(name = "company_name")
+    private String companyName;
+
+    @Column(name = "report_no")
+    private String reportNumber;
+
+    @Column(name = "product_type")
+    private String productType;
+
+    @Column(name = "safety_inspection_standard")
+    private String safetyInspectionStandard;
+
+    @Column(name = "upper_item")
+    private String upperItem;
+
+    @Column(name = "item")
+    private String item;
+
+    @Column(name = "propose", length = Integer.MAX_VALUE)
+    private String propose;
+
+    @Column(name = "weight")
+    private String weight;
+
+    @Column(name = "usage", length = Integer.MAX_VALUE)
+    private String usage;
+
+    @Column(name = "usage_precaution", length = Integer.MAX_VALUE)
+    private String usagePrecaution;
+
+    @Column(name = "first_aid", length = Integer.MAX_VALUE)
+    private String firstAid;
+
+    @Column(name = "main_substance", length = Integer.MAX_VALUE)
+    private String mainSubstance;
+
+    @Column(name = "allergic_substance", length = Integer.MAX_VALUE)
+    private String allergicSubstance;
+
+    @Column(name = "other_substance", length = Integer.MAX_VALUE)
+    private String otherSubstance;
+
+    @Column(name = "preservative", length = Integer.MAX_VALUE)
+    private String preservative;
+
+    @Column(name = "surfactant", length = Integer.MAX_VALUE)
+    private String surfactant; // 계면활성제
+
+    @Column(name = "fluorescent_whitening", length = Integer.MAX_VALUE)
+    private String fluorescentWhitening;
+
+    @Column(name = "manufacture_type")
+    private String manufactureType;
+
+    @Column(name = "manufacture_method")
+    private String manufactureMethod;
+
+    @Column(name = "manufacture_country")
+    private String manufactureCountry;
+
 
     @ManyToOne
     @JoinColumn(name = "brand_no")
@@ -54,26 +113,88 @@ public class Product extends BaseEntity {
             String productName,
             String barcode,
             String description,
-            String declareNo,
+            String reportNumber,
             Boolean isViolation,
-            Integer viewCount
+            Integer viewCount,
+            String companyName,
+            String productType,
+            String safetyInspectionStandard,
+            String upperItem,
+            String item,
+            String propose,
+            String weight,
+            String usage,
+            String usagePrecaution,
+            String firstAid,
+            String mainSubstance,
+            String allergicSubstance,
+            String otherSubstance,
+            String preservative,
+            String surfactant,
+            String fluorescentWhitening,
+            String manufactureType,
+            String manufactureMethod,
+            String manufactureCountry,
+            Brand brand
     ) {
         this.productName = productName;
         this.barcode = barcode;
         this.description = description;
-        this.declareNo = declareNo;
+        this.reportNumber = reportNumber;
         this.isViolation = isViolation;
         this.viewCount = viewCount;
+        this.brand = brand;
+        this.companyName = companyName;
+        this.productType = productType;
+        this.safetyInspectionStandard = safetyInspectionStandard;
+        this.upperItem = upperItem;
+        this.item = item;
+        this.propose = propose;
+        this.weight = weight;
+        this.usage = usage;
+        this.usagePrecaution = usagePrecaution;
+        this.firstAid = firstAid;
+        this.mainSubstance = mainSubstance;
+        this.allergicSubstance = allergicSubstance;
+        this.otherSubstance = otherSubstance;
+        this.preservative = preservative;
+        this.surfactant = surfactant;
+        this.fluorescentWhitening = fluorescentWhitening;
+        this.manufactureType = manufactureType;
+        this.manufactureMethod = manufactureMethod;
+        this.manufactureCountry = manufactureCountry;
     }
 
-    public static Product of(
-            String productName,
-            String barcode,
-            String description,
-            String declareNo,
-            Boolean isViolation,
-            Integer viewCount
-    ) {
-        return new Product(productName, barcode, description, declareNo, isViolation, viewCount);
+    public static Product of(String productName,
+                             String barcode,
+                             String description,
+                             String reportNumber,
+                             Boolean isViolation,
+                             Integer viewCount,
+                             String companyName,
+                             String productType,
+                             String safetyInspectionStandard,
+                             String upperItem,
+                             String item,
+                             String propose,
+                             String weight,
+                             String usage,
+                             String usagePrecaution,
+                             String firstAid,
+                             String mainSubstance,
+                             String allergicSubstance,
+                             String otherSubstance,
+                             String preservative,
+                             String surfactant,
+                             String fluorescentWhitening,
+                             String manufactureType,
+                             String manufactureMethod,
+                             String manufactureCountry,
+                             Brand brand) {
+        return new Product(productName, barcode, description, reportNumber, isViolation, viewCount, companyName,
+                productType, safetyInspectionStandard, upperItem,
+                item, propose, weight, usage, usagePrecaution, firstAid, mainSubstance, allergicSubstance,
+                otherSubstance, preservative, surfactant,
+                fluorescentWhitening, manufactureType, manufactureMethod, manufactureCountry, brand);
     }
 }
--- module-domain/src/main/java/com/kernel360/product/repository/ProductRepository.java
+++ module-domain/src/main/java/com/kernel360/product/repository/ProductRepository.java
@@ -1,14 +1,19 @@
 package com.kernel360.product.repository;
 
 import com.kernel360.product.entity.Product;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     @Query(value = "SELECT * FROM Product WHERE product_name LIKE CONCAT('%', :keyword, '%') OR barcode LIKE CONCAT('%', :keyword, '%') OR description LIKE CONCAT('%', :keyword, '%')", nativeQuery = true)
     List<Product> findByKeyword(@Param("keyword") String keyword);
+
+    @Query(value = "SELECT p FROM Product  p WHERE p.productName = :productName AND p.reportNumber = :reportNumber")
+    Optional<Product> findProductByProductNameAndReportNumber(@Param("productName") String productName,
+                                                              @Param("reportNumber") String reportNumber);
 }
