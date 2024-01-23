465a58b52e39c49f801c1452373aac0478ad141f
김찬규 <kjkj5868@gmail.com>
2024-01-23 15:31:18 +0900

    chore :: 스케쥴러 시간 설정 변경, 사용하지 않는 Job의 스케쥴 설정 삭제

--- module-batch/src/main/java/com/kernel360/modulebatch/scheduler/ReportedProductScheduler.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/scheduler/ReportedProductScheduler.java
@@ -16,21 +16,19 @@ import org.springframework.stereotype.Component;
 @Component
 @Profile({"local", "prod"})
 public class ReportedProductScheduler {
-    private static final long FIXED_DELAY_WEEK = 60 * 60 * 24 * 7 * 1000L;
-    private static final long FIXED_DELAY_HALF_HOUR = 60 * 30 * 1000L;
-    private final Job fetchReportedProductJob;
+
     private final Job importProductFromReportedProductJob;
     private final Job fetchReportedProductFromBrandJob;
     private final Job fetchReportedProductDetailJob;
     private final JobLauncher jobLauncher;
 
     @Autowired
-    public ReportedProductScheduler(@Qualifier("fetchReportedProductJob") Job fetchReportedProductJob,
-                                    @Qualifier("importProductFromReportedProductJob") Job importProductFromReportedProductJob,
-                                    @Qualifier("fetchReportedProductDetailJob") Job fetchReportedProductDetailJob,
-                                    @Qualifier("fetchReportedProductFromBrandJob") Job fetchReportedProductFromBrandJob,
-                                    JobLauncher jobLauncher) {
-        this.fetchReportedProductJob = fetchReportedProductJob;
+    public ReportedProductScheduler(
+            @Qualifier("importProductFromReportedProductJob") Job importProductFromReportedProductJob,
+            @Qualifier("fetchReportedProductDetailJob") Job fetchReportedProductDetailJob,
+            @Qualifier("fetchReportedProductFromBrandJob") Job fetchReportedProductFromBrandJob,
+            JobLauncher jobLauncher) {
+
         this.importProductFromReportedProductJob = importProductFromReportedProductJob;
         this.fetchReportedProductDetailJob = fetchReportedProductDetailJob;
         this.fetchReportedProductFromBrandJob = fetchReportedProductFromBrandJob;
@@ -38,16 +36,9 @@ public class ReportedProductScheduler {
     }
 
     /**
-     * 초록누리 API 를 통해 신고대상 생활화학제품 목록 정보 읽어오기
-     */
-    public void executeFetchReportedProductJob() {
-        executeJob(fetchReportedProductJob);
-    }
-
-    /**
      * Brand 테이블의 brand 정보에 매칭되는 신고대상 생활화학제품 목록 정보 읽어오기
      */
-    @Scheduled(initialDelay = 1000L, fixedDelay = FIXED_DELAY_WEEK)
+    @Scheduled(cron = "0 0 1 * * WED", zone = "Asia/Seoul")
     public void fetchReportedProductFromBrandJob() {
         executeJob(fetchReportedProductFromBrandJob);
     }
@@ -55,15 +46,15 @@ public class ReportedProductScheduler {
     /**
      * 초록누리 API 를 통해 신고대상 생활화학제품 상세 정보 읽어오기
      */
-    @Scheduled(initialDelay = FIXED_DELAY_HALF_HOUR * 2, fixedDelay = FIXED_DELAY_WEEK)
+    @Scheduled(cron = "0 0 2 * * WED", zone = "Asia/Seoul")
     public void executeFetchReportedProductDetailJob() {
         executeJob(fetchReportedProductDetailJob);
     }
 
     /**
-     * Reported Product 테이블에서 Product 테이블로 제품 정보 옮기기
+     * Reported Product 테이블에서 Product 테이블로 필요한 제품 정보 옮기기
      */
-    @Scheduled(initialDelay = FIXED_DELAY_HALF_HOUR * 3, fixedDelay = FIXED_DELAY_WEEK)
+    @Scheduled(cron = "0 30 2 * * WED", zone = "Asia/Seoul")
     public void executeImportProductJob() {
         executeJob(importProductFromReportedProductJob);
     }
