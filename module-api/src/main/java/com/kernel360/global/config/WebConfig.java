64259e109d219a41bd8daedf2125bf4634c219cc
Younglong <kyl8555@gmail.com>
2024-01-30 16:23:58 +0900

    fix: WebConfig 파일 수정
    - 개발 서버에서 /docs 하위 html 파일에 접근 시 500 에러 발생
    - 오류 방지를 위해 기본 경로 지정

--- module-api/src/main/java/com/kernel360/global/config/WebConfig.java
+++ module-api/src/main/java/com/kernel360/global/config/WebConfig.java
@@ -1,8 +1,9 @@
-package com.kernel360.main.config;
+package com.kernel360.global.config;
 
 import com.kernel360.main.conveter.StringToSortConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToSortConverter());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico").addResourceLocations("classpath:/static/");
     }
 }
