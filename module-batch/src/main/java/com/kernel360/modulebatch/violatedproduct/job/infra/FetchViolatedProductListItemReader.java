03930fd1950f600ae0661acf68f0cab4344f29fc
김찬규 <kjkj5868@gmail.com>
2024-02-02 18:49:56 +0900

    feat :: 위반제품목록을 불러오는 배치 작업 구현

---
+++ module-batch/src/main/java/com/kernel360/modulebatch/violatedproduct/job/infra/FetchViolatedProductListItemReader.java
@@ -0,0 +1,58 @@
+package com.kernel360.modulebatch.violatedproduct.job.infra;
+
+import com.kernel360.modulebatch.violatedproduct.client.ViolatedProductListClient;
+import com.kernel360.modulebatch.violatedproduct.dto.ViolatedProductDto;
+import com.kernel360.modulebatch.violatedproduct.service.ViolatedProductService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.annotation.BeforeStep;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@StepScope
+@Component
+@RequiredArgsConstructor
+public class FetchViolatedProductListItemReader implements ItemReader<List<ViolatedProductDto>> {
+    private static int MAX_PAGES_PER_JOB;
+
+    private final ViolatedProductListClient client;
+
+    private final ViolatedProductService service;
+
+    private int maxPageNumber;
+    private int nextPage = -1;
+
+    @BeforeStep
+    public void beforeStep() {
+        MAX_PAGES_PER_JOB = 100;
+    }
+
+    @Override
+    public List<ViolatedProductDto> read()
+            throws Exception {
+        nextPage++;
+        if (nextPage == maxPageNumber + 1 || MAX_PAGES_PER_JOB-- <= 0) {
+            return null;
+        }
+
+        String xmlResponse = fetchXmlResponse();
+
+        maxPageNumber = service.getTotalPageCount(xmlResponse);
+
+        return service.deserializeXml2ListDto(xmlResponse)
+                      .violatedProductDtoList();
+    }
+
+    private String fetchXmlResponse() {
+        log.info("Fetch Page = {}", nextPage);
+        String xmlResponse = client.getXmlResponse("07", nextPage);
+        log.info("Response accepted : {}", xmlResponse);
+        return xmlResponse;
+    }
+}
