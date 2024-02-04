f7a303fc0ad07e559260cafb3742f89a85ac11db
gunsight <gunsight777@naver.com>
2024-02-05 02:15:14 +0900

    * DB ORM Config 패키지 이동 (git 파일 추적 끊김으로 인해 분할 commit)

---
+++ module-infra/src/main/java/com/kernel360/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.kernel360.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
