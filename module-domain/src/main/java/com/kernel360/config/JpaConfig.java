00617a4f8644f5722f43dbe4427106e372ff15c7
gunsight <gunsight777@naver.com>
2024-02-05 02:00:17 +0900

    * DB ORM Config 패키지 이동

--- module-domain/src/main/java/com/kernel360/config/JpaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.kernel360.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@Configuration
-@EnableJpaAuditing
-public class JpaConfig {
-}
