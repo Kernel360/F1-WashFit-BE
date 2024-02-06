d28c73495961ffa8aa0df3a0757ad92f79a9d31b
Younglong <kyl8555@gmail.com>
2024-02-06 17:26:53 +0900

    chore: JasyptEncryptor.java 전체 주석 처리
    - 실제 암호키 만들어야할 때 주석 해제해서 사용
    - 개발 서버에서 사용하지 않고 로컬 서버 돌릴 때 수정해야하는 이슈 발생해서 주석 처리

--- module-common/src/main/java/com/kernel360/utils/jasypt/JasyptEncryptor.java
+++ module-common/src/main/java/com/kernel360/utils/jasypt/JasyptEncryptor.java
@@ -1,30 +1,30 @@
-package com.kernel360.utils.jasypt;
-
-import org.jasypt.encryption.StringEncryptor;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class JasyptEncryptor implements CommandLineRunner {
-    private final StringEncryptor stringEncryptor;
-
-    public JasyptEncryptor(@Qualifier("washpediaEncryptorBean") StringEncryptor stringEncryptor) {
-        this.stringEncryptor = stringEncryptor;
-    }
-
-    public static void main(String[] args) {
-        SpringApplication.run(JasyptEncryptor.class, args);
-    }
-
-    @Override
-    public void run(String... args) {
-        String url = "암호화 할 문자열을 입력하세요";
-        String encryptedUrl = stringEncryptor.encrypt(url);
-
-        System.out.println("[Original] : " + url);
-        System.out.println("[Encrypted] : " + "ENC(" + encryptedUrl + ")");
-        System.out.println("[Decrypted] : " + stringEncryptor.decrypt(encryptedUrl));
-    }
-}
+//package com.kernel360.utils.jasypt;
+//
+//import org.jasypt.encryption.StringEncryptor;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class JasyptEncryptor implements CommandLineRunner {
+//    private final StringEncryptor stringEncryptor;
+//
+//    public JasyptEncryptor(@Qualifier("washpediaEncryptorBean") StringEncryptor stringEncryptor) {
+//        this.stringEncryptor = stringEncryptor;
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(JasyptEncryptor.class, args);
+//    }
+//
+//    @Override
+//    public void run(String... args) {
+//        String url = "암호화 할 문자열을 입력하세요";
+//        String encryptedUrl = stringEncryptor.encrypt(url);
+//
+//        System.out.println("[Original] : " + url);
+//        System.out.println("[Encrypted] : " + "ENC(" + encryptedUrl + ")");
+//        System.out.println("[Decrypted] : " + stringEncryptor.decrypt(encryptedUrl));
+//    }
+//}
