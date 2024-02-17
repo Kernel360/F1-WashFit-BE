ddd61fbf8cc4452a2c83e08e54d7662a45332948
YunNote <zzdd1558@gmail.com>
2024-02-17 20:25:24 +0900

    feature: 만약 버전은 어떻게 관리할 수 있을까 ?

--- module-api/src/main/java/com/kernel360/commoncode/controller/CommonCodeController.java
+++ module-api/src/main/java/com/kernel360/commoncode/controller/CommonCodeController.java
@@ -20,6 +20,8 @@ import static com.kernel360.commoncode.code.CommonCodeBusinessCode.GET_COMMON_CO
 public class CommonCodeController {
 
     private final CommonCodeService commonCodeService;
+
+    //
     @GetMapping("/{codeName}")
     public ResponseEntity<ApiResponse<List<CommonCodeDto>>> getCommonCode (@PathVariable String codeName){
         List<CommonCodeDto> codes = commonCodeService.getCodes(codeName);
