d00e2cffbe200463248a51ef6a0cc0d1782b6f19
gunsight1 <gunsight777@gmail.com>
2024-02-29 18:36:38 +0900

    Revert "* 테이블 컬럼 추가시 존재여부를 판단 한 후 수행하도록 PLSQL로 작성"
    This reverts commit 8b65787b9053b8763e07f458963b9f1c5d9c9958.

--- module-api/src/main/resources/db/migration/V1.0.9__add_clientIP_to_auth.sql
+++ module-api/src/main/resources/db/migration/V1.0.9__add_clientIP_to_auth.sql
@@ -1,11 +1,2 @@
-DO $$
-BEGIN
-    IF NOT EXISTS (
-        SELECT 1
-        FROM information_schema.columns
-        WHERE table_name = 'auth'
-        AND column_name = 'client_ip'
-    ) THEN
-ALTER TABLE auth ADD COLUMN client_ip VARCHAR;
-END IF;
-END $$;
+ALTER TABLE auth
+    ADD COLUMN client_ip varchar;
