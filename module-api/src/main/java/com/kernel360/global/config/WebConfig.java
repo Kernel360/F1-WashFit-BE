ffde7a61c118c9b8b4a406bb7270e0784c2099ae
gunsight1 <gunsight777@gmail.com>
2024-02-29 20:07:44 +0900

    * 프론트 로컬 개발 환경용 localhost:3000 추가

--- module-api/src/main/java/com/kernel360/global/config/WebConfig.java
+++ module-api/src/main/java/com/kernel360/global/config/WebConfig.java
@@ -26,7 +26,7 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true)
-                .allowedOrigins("https://www.washfit.site", "https://dev.washfit.site")
+                .allowedOrigins("https://www.washfit.site", "https://dev.washfit.site", "http://localhost:3000")
                 .maxAge(3600);
     }
 }
