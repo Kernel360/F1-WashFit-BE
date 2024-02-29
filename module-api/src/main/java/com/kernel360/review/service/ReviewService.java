3d1e15a090734f1aebc9ffb600cc2a696dc4b8b2
Younglong <kyl8555@gmail.com>
2024-02-29 23:43:23 +0900

    feat: 리뷰 단건 조회 null 관련 처리

--- module-api/src/main/java/com/kernel360/review/service/ReviewService.java
+++ module-api/src/main/java/com/kernel360/review/service/ReviewService.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 @Slf4j
 @Service
@@ -34,9 +35,14 @@ public class ReviewService {
 
     @Transactional(readOnly = true)
     public ReviewDto getReview(Long reviewNo) {
-        log.info("리뷰 단건 조회 -> review_no {}", reviewNo);
+        Review review = reviewRepository.findByReviewNo(reviewNo);
 
-        return ReviewDto.from(reviewRepository.findByReviewNo(reviewNo));
+        if (Objects.isNull(review)) {
+            throw new BusinessException(ReviewErrorCode.NOT_FOUND_REVIEW);
+        }
+
+        log.info("리뷰 단건 조회 -> review_no {}", reviewNo);
+        return ReviewDto.from(review);
     }
 
     @Transactional
