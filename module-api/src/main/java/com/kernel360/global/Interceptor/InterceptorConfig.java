2e2bab518e68835511a19e18a6b8a7590e6bdb91
cgk95 <kjkj5868@gmail.com>
2024-03-01 19:02:27 +0900

    develop 브랜치 최신 변경사항 squash

--- module-api/src/main/java/com/kernel360/global/Interceptor/InterceptorConfig.java
+++ module-api/src/main/java/com/kernel360/global/Interceptor/InterceptorConfig.java
@@ -13,8 +13,10 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(acceptInterceptor)
-                .addPathPatterns("/auth/**"); //** 인증 JWT 토큰 관련 **//
-//                .addPathPatterns("/mypage/**");
+                .addPathPatterns("/auth/**") //** 인증 JWT 토큰 관련 **//
+                .addPathPatterns("/reviews/**")
+                .addPathPatterns("/likes/**")
+                .addPathPatterns("/mypage/**");
                 //.excludePathPatterns("/public/**"); // 제외할 URL 패턴
     }
 
