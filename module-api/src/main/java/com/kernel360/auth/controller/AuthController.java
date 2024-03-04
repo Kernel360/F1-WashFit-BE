ac7885bf44a3cea1b2e030da0640e2c8425a4f07
gunsight1 <gunsight777@gmail.com>
2024-03-04 14:47:31 +0900

    * 토큰갱신 재발급 요청 맵핑방식 변경

--- module-api/src/main/java/com/kernel360/auth/controller/AuthController.java
+++ module-api/src/main/java/com/kernel360/auth/controller/AuthController.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,7 +23,7 @@ public class AuthController {
 
     private final AuthService authService;
 
-    @GetMapping("/reissuanceJWT")
+    @PostMapping("/reissuanceJWT")
     public ResponseEntity<ApiResponse<AuthDto>> reissuanceJWT(HttpServletRequest request){
         log.info("Current Client IP :: "+getClientIP(request));
         return ApiResponse.toResponseEntity(SUCCESS_REQUEST_REGENERATED_JWT, AuthDto.of(authService.generateTokenAndSaveAuth(request)));
