550edeac57b03702bd671972e9cac009f104e42c
cgk95 <kjkj5868@gmail.com>
2024-03-05 17:04:38 +0900

    feat :: Admin 모듈의 review 조회, 삭제 기능 명세 구현
    등록, 수정 기능은 회원 정보가 필요(admin의 회원 정보가 아니더라도 요청-응답용 DTO 를 채우기 위해서) 하기도 하고, 관리자 계정에서 리뷰를 직접 등록하고 업데이트할 이유가 없다고 생각하여 구현하지 않았습니다.

---
+++ module-admin/src/main/java/com/kernel360/review/controller/ReviewController.java
@@ -0,0 +1,68 @@
+package com.kernel360.review.controller;
+
+import com.kernel360.response.ApiResponse;
+import com.kernel360.review.code.ReviewBusinessCode;
+import com.kernel360.review.dto.AdminReviewDto;
+import com.kernel360.review.service.ReviewServiceImpl;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/admin/reviews")
+public class ReviewController {
+
+    private final ReviewServiceImpl reviewService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<AdminReviewDto>>> getAllReviews(
+            @RequestParam(name = "sortBy", defaultValue = "reviewNo", required = false) String sortBy,
+            Pageable pageable) {
+
+        return ApiResponse.toResponseEntity(ReviewBusinessCode.SUCCESS_GET_REVIEWS,
+                reviewService.getReviews(sortBy, pageable));
+    }
+
+    @GetMapping("/product")
+    public ResponseEntity<ApiResponse<Page<AdminReviewDto>>> getReviewsByProduct(
+            @RequestParam(name = "productNo") Long productNo,
+            @RequestParam(name = "sortBy", defaultValue = "reviewNo", required = false) String sortBy,
+            Pageable pageable) {
+
+        return ApiResponse.toResponseEntity(ReviewBusinessCode.SUCCESS_GET_REVIEWS,
+                reviewService.getReviewsByProduct(productNo, sortBy, pageable));
+    }
+
+    @GetMapping("/member")
+    public ResponseEntity<ApiResponse<Page<AdminReviewDto>>> getReviewsByMember(
+            @RequestParam(name = "memberNo") Long memberNo,
+            @RequestParam(name = "sortBy", defaultValue = "reviewNo", required = false) String sortBy,
+            Pageable pageable) {
+
+        return ApiResponse.toResponseEntity(ReviewBusinessCode.SUCCESS_GET_REVIEWS,
+                reviewService.getReviewsByMember(memberNo, sortBy, pageable));
+    }
+
+    @GetMapping("/{reviewNo}")
+    public ResponseEntity<ApiResponse<AdminReviewDto>> getReview(@PathVariable Long reviewNo) {
+
+        return ApiResponse.toResponseEntity(ReviewBusinessCode.SUCCESS_GET_REVIEW, reviewService.getReview(reviewNo));
+    }
+
+    @DeleteMapping("/{reviewNo}")
+    public <T> ResponseEntity<ApiResponse<T>> deleteReview(@PathVariable Long reviewNo) {
+        reviewService.deleteReview(reviewNo);
+
+        return ApiResponse.toResponseEntity(ReviewBusinessCode.SUCCESS_DELETE_REVIEW);
+    }
+}
