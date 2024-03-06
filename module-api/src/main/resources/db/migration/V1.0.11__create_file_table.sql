2903c7197fdc04136225593d321addc377b6cd29
Younglong <kyl8555@gmail.com>
2024-03-06 10:57:23 +0900

    feat: file 테이블 컬럼 제약조건 추가

--- module-api/src/main/resources/db/migration/V1.0.11__create_file_table.sql
+++ module-api/src/main/resources/db/migration/V1.0.11__create_file_table.sql
@@ -2,8 +2,8 @@ CREATE TABLE IF NOT EXISTS file
 (
     file_no        BIGSERIAL PRIMARY KEY,
     file_name      VARCHAR(255) NOT NULL,
-    file_key       VARCHAR(255) NOT NULL,
-    file_url       VARCHAR(500) NOT NULL,
+    file_key       VARCHAR(255) NOT NULL UNIQUE,
+    file_url       VARCHAR(500) NOT NULL UNIQUE,
     reference_type VARCHAR(50),
     reference_no   BIGINT,
     created_at     DATE         NOT NULL,
