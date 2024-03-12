5f04159e5c1e345d2f9c3eb16c39a4ff2a65e05a
gunsight1 <gunsight777@gmail.com>
2024-03-12 18:43:07 +0900

    * BBS 게시판 서비스 작성

--- module-api/src/main/java/com/kernel360/bbs/service/BBSService.java
+++ module-api/src/main/java/com/kernel360/bbs/service/BBSService.java
@@ -1,19 +1,48 @@
 package com.kernel360.bbs.service;
 
 import com.kernel360.bbs.dto.BBSDto;
+import com.kernel360.bbs.dto.BBSListDto;
+import com.kernel360.bbs.entity.BBS;
 import com.kernel360.bbs.repository.BBSRepository;
+import com.kernel360.member.dto.MemberDto;
+import com.kernel360.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BBSService {
     private final BBSRepository bbsRepository;
+    private final MemberService memberService;
 
-    public Page<BBSDto> getBBSWithCondition(String bbsType, String keyword, Pageable pageable) {
+    public Page<BBSListDto> getBBSWithCondition(String type, String keyword, Pageable pageable) {
 
-        return bbsRepository.getBBSWithCondition(bbsType, keyword, pageable).map(BBSDto::from);
+        return bbsRepository.getBBSWithCondition(type, keyword, pageable);
+    }
+
+    public BBSDto getBBSView(Long bbsNo) {
+
+        return BBSDto.from(bbsRepository.findOneByBbsNo(bbsNo));
+    }
+
+    public Page<BBSDto> getBBSReply(Long upperNo, Pageable pageable) {
+
+        return bbsRepository.findAllByUpperNo(upperNo, pageable).map(BBSDto::from);
+    }
+
+    @Transactional
+    public void saveBBS(BBSDto bbsDto, String id) {
+
+        MemberDto memberDto = memberService.findByMemberId(id);
+
+        bbsRepository.save(BBS.save(bbsDto.bbsNo(), bbsDto.upperNo(), bbsDto.type(), bbsDto.title(), bbsDto.contents(), true, 0L, memberDto.toEntity()));
+    }
+
+    @Transactional
+    public void deleteBBS(Long bbsNo) {
+        bbsRepository.deleteByBbsNo(bbsNo);
     }
 }
