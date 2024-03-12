35b8a55d0952212d2751471d80d9c60ca0db92aa
gunsight1 <gunsight777@gmail.com>
2024-03-12 18:41:15 +0900

    * BBS 컨트롤러 작성, 목록 상세 등록 수정 삭제

--- module-api/src/main/java/com/kernel360/bbs/controller/BBSController.java
+++ module-api/src/main/java/com/kernel360/bbs/controller/BBSController.java
@@ -2,6 +2,7 @@ package com.kernel360.bbs.controller;
 
 import com.kernel360.bbs.code.BBSBusinessCode;
 import com.kernel360.bbs.dto.BBSDto;
+import com.kernel360.bbs.dto.BBSListDto;
 import com.kernel360.bbs.service.BBSService;
 import com.kernel360.response.ApiResponse;
 import lombok.RequiredArgsConstructor;
@@ -16,27 +17,48 @@ public class BBSController {
     private final BBSService bbsService;
 
     @GetMapping("/bbs")
-    public ResponseEntity<ApiResponse<Page<BBSDto>>> getBBS(
-            @RequestParam(value = "bbsType", defaultValue = "")String bbsType,
+    public ResponseEntity<ApiResponse<Page<BBSListDto>>> getBBS(
+            @RequestParam(value = "type", defaultValue = "") String type,
             @RequestParam(value = "keyword", required = false) String keyword, Pageable pageable
     ){
-        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS, bbsService.getBBSWithCondition(bbsType, keyword, pageable));
+        Page<BBSListDto> result = bbsService.getBBSWithCondition(type, keyword, pageable);
+
+        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS, result);
+    }
+
+    @GetMapping("/bbs/{bbsNo}")
+    public ResponseEntity<ApiResponse<BBSDto>> getBBSView(@PathVariable Long bbsNo){
+
+        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS_VIEW, bbsService.getBBSView(bbsNo));
+    }
+
+    @GetMapping("/bbs/reply")
+    public ResponseEntity<ApiResponse<Page<BBSDto>>> getBBSReply(@RequestParam Long upperNo, Pageable pageable){
+
+        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS_VIEW, bbsService.getBBSReply(upperNo, pageable));
     }
 
     @PostMapping("/bbs")
-    public ResponseEntity<ApiResponse<BBSDto>> saveBBS(@RequestBody BBSDto bbsDto){
+    public ResponseEntity<ApiResponse<BBSDto>> saveBBS(@RequestBody BBSDto bbsDto, @RequestHeader("Id") String id){
 
-        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS);
+        bbsService.saveBBS(bbsDto, id);
+
+        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_CREATED_BBS);
     }
 
     @PatchMapping("/bbs")
-    public ResponseEntity<ApiResponse<BBSDto>> modifyBBS(@RequestBody BBSDto bbsDto){
+    public ResponseEntity<ApiResponse<BBSDto>> modifyBBS(@RequestBody BBSDto bbsDto, @RequestHeader("Id") String id){
 
-        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS);
+        bbsService.saveBBS(bbsDto, id);
+
+        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_MODIFIED_BBS);
     }
 
     @DeleteMapping("/bbs")
-    public ResponseEntity<ApiResponse<BBSDto>> deleteBBS(@RequestBody BBSDto bbsDto){
-        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_GET_BBS);
+    public ResponseEntity<ApiResponse<BBSDto>> deleteBBS(@RequestParam Long bbsNo){
+
+        bbsService.deleteBBS(bbsNo);
+
+        return ApiResponse.toResponseEntity(BBSBusinessCode.SUCCESS_REQUEST_DELETE_BBS);
     }
 }
