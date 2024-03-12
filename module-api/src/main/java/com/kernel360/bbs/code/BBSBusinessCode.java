3088c6f540c4ec07663b2c06a70262d9cb6e4e16
gunsight1 <gunsight777@gmail.com>
2024-03-12 18:40:54 +0900

    * BBS 응답코드 작성

--- module-api/src/main/java/com/kernel360/bbs/code/BBSBusinessCode.java
+++ module-api/src/main/java/com/kernel360/bbs/code/BBSBusinessCode.java
@@ -8,9 +8,10 @@ import org.springframework.http.HttpStatus;
 public enum BBSBusinessCode implements BusinessCode {
 
     SUCCESS_REQUEST_GET_BBS(HttpStatus.OK.value(), "BBC001", "게시판 목록 조회 성공"),
-    SUCCESS_REQUEST_CREATED_BBS(HttpStatus.CREATED.value(), "BBC002", "게시글 작성 성공"),
-    SUCCESS_REQUEST_MODIFIED_BBS(HttpStatus.OK.value(), "BBC003", "게시글 수정 성공"),
-    SUCCESS_REQUEST_DELETE_BBS(HttpStatus.OK.value(), "BBC004", "게시글 삭제 성공");
+    SUCCESS_REQUEST_GET_BBS_VIEW(HttpStatus.OK.value(), "BBC002", "게시판 상세 조회 성공"),
+    SUCCESS_REQUEST_CREATED_BBS(HttpStatus.CREATED.value(), "BBC003", "게시글 작성 성공"),
+    SUCCESS_REQUEST_MODIFIED_BBS(HttpStatus.OK.value(), "BBC004", "게시글 수정 성공"),
+    SUCCESS_REQUEST_DELETE_BBS(HttpStatus.OK.value(), "BBC005", "게시글 삭제 성공");
 
     private final int status;
     private final String code;
@@ -18,16 +19,14 @@ public enum BBSBusinessCode implements BusinessCode {
 
     @Override
     public int getStatus() {
-        return 0;
+        return status;
     }
 
     @Override
-    public String getCode() {
-        return null;
-    }
+    public String getCode() { return code; }
 
     @Override
     public String getMessage() {
-        return null;
+        return message;
     }
 }
--- module-api/src/main/java/com/kernel360/bbs/code/BBSErrorCode.java
+++ module-api/src/main/java/com/kernel360/bbs/code/BBSErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 public enum BBSErrorCode implements ErrorCode {
 
     FAILED_GET_BBS_LIST(HttpStatus.NO_CONTENT.value(), "BMC001", "게시판 목록을 찾을 수 없음."),
-    FAILED_GET_BBS_ONE(HttpStatus.NO_CONTENT.value(), "BMC002", "게시글을 찾을 수 없음.");
+    FAILED_GET_BBS_VIEW(HttpStatus.NO_CONTENT.value(), "BMC002", "게시글을 찾을 수 없음.");
 
     private final int status;
     private final String code;
@@ -16,16 +16,16 @@ public enum BBSErrorCode implements ErrorCode {
 
     @Override
     public int getStatus() {
-        return 0;
+        return status;
     }
 
     @Override
     public String getCode() {
-        return null;
+        return code;
     }
 
     @Override
     public String getMessage() {
-        return null;
+        return message;
     }
 }
