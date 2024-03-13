d77d59901444b4004706fd18fe05a8359b188143
cgk95 <kjkj5868@gmail.com>
2024-03-13 11:33:41 +0900

    chore :: BaseStepExecutionListener 사용하도록 변경

--- module-batch/src/main/java/com/kernel360/modulebatch/violatedproduct/job/core/FetchViolatedProductDetailJobConfig.java
+++ module-batch/src/main/java/com/kernel360/modulebatch/violatedproduct/job/core/FetchViolatedProductDetailJobConfig.java
@@ -2,15 +2,13 @@ package com.kernel360.modulebatch.violatedproduct.job.core;
 
 import com.kernel360.ecolife.entity.ViolatedProduct;
 import com.kernel360.modulebatch.global.BaseJobExecutionListener;
+import com.kernel360.modulebatch.global.BaseStepExecutionListener;
 import com.kernel360.modulebatch.violatedproduct.job.infra.FetchViolatedProductDetailItemProcessor;
 import jakarta.persistence.EntityManagerFactory;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepExecution;
-import org.springframework.batch.core.StepExecutionListener;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
@@ -35,6 +33,7 @@ public class FetchViolatedProductDetailJobConfig {
     public static final String JOB_NAME = "FetchViolatedProductDetailJob";
     private final FetchViolatedProductDetailItemProcessor fetchViolatedProductDetailItemProcessor;
     private final BaseJobExecutionListener baseJobExecutionListener;
+    private final BaseStepExecutionListener baseStepExecutionListener;
     private final EntityManagerFactory emf;
 
     @Bean
@@ -57,7 +56,7 @@ public class FetchViolatedProductDetailJobConfig {
                 .reader(fetchViolatedProductDetailJpaCursorItemReader())
                 .processor(fetchViolatedProductDetailItemProcessor)
                 .writer(fetchViolatedProductDetailJpaItemWriter())
-                .listener(new FetchViolatedProductDetailStepListener())
+                .listener(baseStepExecutionListener)
                 .faultTolerant()
                 .build();
     }
@@ -82,25 +81,4 @@ public class FetchViolatedProductDetailJobConfig {
                 .entityManagerFactory(emf)
                 .build();
     }
-
-    //-- Execution Listener --//
-    public static class FetchViolatedProductDetailStepListener implements StepExecutionListener {
-        @Override
-        public void beforeStep(StepExecution stepExecution) {
-            log.info("{} starts", stepExecution.getStepName());
-        }
-
-        @Override
-        public ExitStatus afterStep(StepExecution stepExecution) {
-            log.info("StepExecutionListener - afterStep, step name: {}, status: {}", stepExecution.getStepName(),
-                    stepExecution.getStatus());
-            log.info(
-                    "StepExecutionListener - ReadCount: {}, WriteCount: {}, FilterCount: {}, ReadSkipCount: {}, ProcessSkipCount: {}, WriteSkipCount: {}",
-                    stepExecution.getReadCount(), stepExecution.getWriteCount(), stepExecution.getFilterCount(),
-                    stepExecution.getReadSkipCount(), stepExecution.getProcessSkipCount(),
-                    stepExecution.getWriteSkipCount());
-            return StepExecutionListener.super.afterStep(stepExecution);
-        }
-    }
-
 }
