00ab661ba93e744911bd50088836ca97884a9858
Younglong <kyl8555@gmail.com>
2024-03-14 22:04:33 +0900

    fix: 이전 PR에서 빌드 오류난 내용 수정

--- module-domain/src/main/java/com/kernel360/member/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kernel360.member.repository;
-
-import com.kernel360.member.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Long> {
-}
