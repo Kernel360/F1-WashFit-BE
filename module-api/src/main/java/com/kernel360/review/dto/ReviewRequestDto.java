cb33e8e8c4740c7c9baca3f4bc8ad9c5d85b8eb2
Younglong <kyl8555@gmail.com>
2024-03-15 10:17:39 +0900

    feat: dto -> 비속어 메시지 적용

--- module-api/src/main/java/com/kernel360/review/dto/ReviewRequestDto.java
+++ module-api/src/main/java/com/kernel360/review/dto/ReviewRequestDto.java
@@ -1,5 +1,6 @@
 package com.kernel360.review.dto;
 
+import com.kernel360.code.common.ValidationMessage;
 import com.kernel360.global.annotation.BadWordFilter;
 import com.kernel360.member.entity.Member;
 import com.kernel360.product.entity.Product;
@@ -16,10 +17,8 @@ public record ReviewRequestDto(Long reviewNo,
                                Long productNo,
                                Long memberNo,
                                BigDecimal starRating,
-                               @BadWordFilter
-                               String title,
-                               @BadWordFilter
-                               String contents,
+                               @BadWordFilter(message = ValidationMessage.INVALID_WORD_PARAMETER) String title,
+                               @BadWordFilter(message = ValidationMessage.INVALID_WORD_PARAMETER) String contents,
                                LocalDateTime createdAt,
                                String createdBy,
                                LocalDateTime modifiedAt,
