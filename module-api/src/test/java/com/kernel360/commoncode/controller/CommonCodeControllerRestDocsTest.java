cba85a93df3e4b7afe47d3c5d2802a6d55ae45d4
Younglong <kyl8555@gmail.com>
2024-03-19 17:21:01 +0900

    feat: 테스트 예시 추가
    - 테스트 예시 파일 추가

--- module-api/src/test/java/com/kernel360/commoncode/controller/CommonCodeControllerRestDocsTest.java
+++ module-api/src/test/java/com/kernel360/commoncode/controller/CommonCodeControllerRestDocsTest.java
@@ -1,25 +1,26 @@
 package com.kernel360.commoncode.controller;
 
-import static com.kernel360.common.utils.RestDocumentUtils.getDocumentRequest;
-import static com.kernel360.common.utils.RestDocumentUtils.getDocumentResponse;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.beneathPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
 import com.kernel360.common.ControllerTest;
 import com.kernel360.commoncode.dto.CommonCodeDto;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.web.servlet.ResultActions;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static com.kernel360.common.utils.RestDocumentUtils.getDocumentRequest;
+import static com.kernel360.common.utils.RestDocumentUtils.getDocumentResponse;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 // FIXME :: 삭제 예정 파일입니다
 class CommonCodeControllerRestDocsTest extends ControllerTest {
@@ -64,39 +65,38 @@ class CommonCodeControllerRestDocsTest extends ControllerTest {
 
 
         //then
-        //pathParameters, pathParameters, requestFields, responseFields는 필요 시 각각 작성
         result.andExpect(status().isOk())
               .andDo(document(
                       "commoncode/get-common-codes",
                       getDocumentRequest(),
                       getDocumentResponse(),
-                      pathParameters(
-                              parameterWithName("codeName").description("코드명")
-                      ),
-//                      queryParameters(
-//                              parameterWithName("size").description("size").optional(),
-//                              parameterWithName("page").description("page").optional()
-//                      ),
-//                      requestFields(
-//                              fieldWithPath("codeName").type(JsonFieldType.STRING).description("코드명"),
-//                              fieldWithPath("upperName").type(JsonFieldType.STRING).description("상위 코드명").optional()
-//                      ),
-//                      responseFields(
-//                              fieldWithPath("codeNo").type(JsonFieldType.NUMBER).description("코드번호"),
-//                      )
-                      responseFields(beneathPath("value").withSubsectionId("value"),
-                              fieldWithPath("codeNo").type(JsonFieldType.NUMBER).description("코드번호"),
-                              fieldWithPath("codeName").type(JsonFieldType.STRING).description("코드명"),
-                              fieldWithPath("upperNo").type(JsonFieldType.NUMBER).description("상위 코드번호").optional(),
-                              fieldWithPath("upperName").type(JsonFieldType.STRING).description("상위 코드명").optional(),
-                              fieldWithPath("sortOrder").type(JsonFieldType.NUMBER).description("정렬순서"),
-                              fieldWithPath("isUsed").type(JsonFieldType.BOOLEAN).description("사용여부"),
-                              fieldWithPath("description").type(JsonFieldType.STRING).description("설명"),
-                              fieldWithPath("createdAt").type(JsonFieldType.STRING).description("생성일시"),
-                              fieldWithPath("createdBy").type(JsonFieldType.STRING).description("생성자"),
-                              fieldWithPath("modifiedAt").type(JsonFieldType.STRING).description("수정일시").optional(),
-                              fieldWithPath("modifiedBy").type(JsonFieldType.STRING).description("수정자").optional()
-                      )
-              ));
+                      resource(ResourceSnippetParameters
+                              .builder()
+                              .tag("CommonCode API")
+                              .summary("공통코드 조회")
+                              .pathParameters(
+                                      parameterWithName("codeName").description("코드명")
+                              )
+                              .responseFields(
+                                      fieldWithPath("status").type(JsonFieldType.NUMBER).description("상태"),
+                                      fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+                                      fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+                                      fieldWithPath("value").type(JsonFieldType.ARRAY).description("응답 데이터 리스트"),
+                                      fieldWithPath("value[].codeNo").type(JsonFieldType.NUMBER).description("코드번호"),
+                                      fieldWithPath("value[].codeName").type(JsonFieldType.STRING).description("코드명"),
+                                      fieldWithPath("value[].upperNo").type(JsonFieldType.NUMBER).description("상위 코드번호").optional(),
+                                      fieldWithPath("value[].upperName").type(JsonFieldType.STRING).description("상위 코드명").optional(),
+                                      fieldWithPath("value[].sortOrder").type(JsonFieldType.NUMBER).description("정렬순서"),
+                                      fieldWithPath("value[].isUsed").type(JsonFieldType.BOOLEAN).description("사용여부"),
+                                      fieldWithPath("value[].description").type(JsonFieldType.STRING).description("설명"),
+                                      fieldWithPath("value[].createdAt").type(JsonFieldType.STRING).description("생성일시"),
+                                      fieldWithPath("value[].createdBy").type(JsonFieldType.STRING).description("생성자"),
+                                      fieldWithPath("value[].modifiedAt").type(JsonFieldType.STRING).description("수정일시").optional(),
+                                      fieldWithPath("value[].modifiedBy").type(JsonFieldType.STRING).description("수정자").optional()
+                              )
+                              .requestSchema(Schema.schema("공통코드 조회"))
+                              .responseSchema(Schema.schema("공통코드 조회"))
+                              .build()
+                      )));
     }
 }
